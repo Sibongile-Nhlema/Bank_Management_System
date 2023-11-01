d2d6e5ac3479741e735b5bc30b8ae158955bf062
Sibongile <sibongile.nhlema@gmail.com>
2023-11-01 15:56:03 +0000

    created the bank.h header file

---
+++ bank.h
@@ -0,0 +1,17 @@
+/*this file will have all prototypes needed to run the Bank Application*/
+/*note that access to all libraries is permitted for the initial building of the project*/
+/*In later development, custom functions will replace some builtin function to demonstrate understanding*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <ctype.h>
+#include <locale.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <time.h>
+
+
